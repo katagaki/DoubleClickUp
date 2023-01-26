ead8477e453a6775b09081acc823e477e7e2cfe8
katagaki <68231035+katagaki@users.noreply.github.com>
2023-01-26 14:16:06 +0800

    Initial Commit

---
+++ DoubleClickUp/DoubleClickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleClickUpApp.swift
+//  DoubleClickUp
+//
+//  Created by Justin Xin on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DoubleClickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
